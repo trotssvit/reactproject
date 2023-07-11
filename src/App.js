0d776a5a7bd46c493a571469e9266afb832b423e
Svitlana Trotsenko <trots.svit@gmail.com>
2023-07-11 01:02:49 +0100

    start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchUsers = async() => {
+            try {
+                const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+                setUsers(response.data);
+            } catch (error) {
+                console.error('Error fetching users:', error);
+            }
+        };
+
+        fetchUsers();
+    }, []);
+
+    return ( < div className = "app" >
+        <
+        h1 > User Data: < /h1> <
+        div className = "user-list" > {
+            users.map((user) => ( <
+                div className = "user"
+                key = { user.id } >
+                <
+                h2 > { user.name } < /h2> <
+                p > Email: { user.email } <
+                /p> <
+                p > Phone: { user.phone } < /p>  <
+                p > Website: { user.website } < /p> < /
+                div >
+            ))
+        } <
+        /div> < /
+        div >
+    );
 }
 
 export default App;
